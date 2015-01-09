01837c36c82f00d872f93a9bc422830b81d42b5d
Eyþór Magnússon <eysi09@gmail.com>
2015-01-09 21:44:54 +0000

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuzzwordsApp::Application.config.secret_token = '4d25e04893643363eaf72ff77856cf7222149ab1b1f2240f5927a65fad2deb48757de327a51ac740b7247a270a0be02f8d89eb17d858a59af52942f9bce65fcb'
