0ef94d23f93ae8e4d15a479aa3c01b33f60b2169
Eyþór Magnússon <eysi09@gmail.com>
2015-01-10 15:39:42 +0000

    Configure sequel to work on mac

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -5,7 +5,11 @@ db = db_config['database']
 user = db_config['username']
 password = db_config['password']
 
-config = "jdbc:postgresql://#{host}/#{db}?user=#{user}&password=#{password}"
+if db_config['which_ruby'] == 'ruby'
+  config = "postgres://#{user}:#{password}@#{host}:/#{db}"
+else # using jruby, for windows machine
+  config = "jdbc:postgresql://#{host}/#{db}?user=#{user}&password=#{password}"
+end
 
 DB = Sequel::Model.db = Sequel.connect(config)
 Sequel.default_timezone = :utc
