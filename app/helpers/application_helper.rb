2daee478b26fcf73ee29df8e8b3287af646131cf
Eyþór Magnússon <eysi09@gmail.com>
2015-02-12 12:39:17 +0100

    Set basic file structure using manifests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def include_public_javascripts
+    # The loading order is important and not all files in public/javascripts
+    # are being loaded.
+    # TODO: Figure out with respect to require tree...
+    javascript_include_tag *%w(
+    ).map { |file| "/javascripts/#{file}" }
+  end
 end
