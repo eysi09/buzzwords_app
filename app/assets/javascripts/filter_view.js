b22cea98a466c30acace3103cb779b84cd8b9f96
Eyþór Magnússon <eysi09@gmail.com>
2015-06-03 00:55:08 +0200

    Added proper init data loading + filter view wip code.

--- app/assets/javascripts/filter_view.js
+++ app/assets/javascripts/filter_view.js
@@ -10,14 +10,35 @@ var FilterView = Backbone.View.extend({
     'change .selectpicker' : 'filter'
   },
 
+  /*
+  Data model:
+  general_assemblies_hash = {
+    ga_id: {
+      parties: [],
+      date_from: '',
+      date_to:   '',
+      mp_ids: []
+    }
+  }
+  parties_x_mp_ids = {
+    party: []
+  }
+  visible_parties = find parties by gas, concat and unique
+  visible_mps = mps_by_ga intersect mps_by_parties
+  */
   initialize: function(options) {
     _.bindAll(this,
       'render',
       'filter')
     this.filters_template   = _.template($('#filters-template').html());
-    this.general_assemblies = options.general_assemblies;
-    this.parties            = options.parties;
-    this.mps                = options.mps;
+    this.general_assemblies_hash = options.general_assemblies;
+    this.mps_name_hash      = options.mps_name_hash;
+    this.visible_mp_ids = null;
+    this.visible_parties = null;
+    this.selected_general_assemblies = [];
+    this.selected_parties = [];
+    this.selected_mps = [];
+    this.build_option_values();
     this.render();
   },
 
@@ -38,6 +59,17 @@ var FilterView = Backbone.View.extend({
     $('#date-to').datetimepicker({
       format: 'YYYY-MM-DD'
     });
+  },
+
+  build_option_values: function() {
+    this.visible_parties = [];
+    this.visble_mps = [];
+    _.each(this.data_hash, function(d) {
+      if (_.contains(this.selected_general_assemblies], d.general_assembly_id) {
+        this.visible_parties.push(d.party)
+      }
+    }, this);
+
   },
 
   filter: function() {
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,40 @@ class SearchController < ApplicationController
   layout 'application'
 
   def init_data
+    ga_data_hash = DB[:general_assemblies]
+      .left_join(:members_of_parliament_x_general_assemblies,
+        'members_of_parliament_x_general_assemblies.general_assembly_id = general_assemblies.ordinal')
+      .group_by(:general_assemblies__ordinal,
+        :general_assemblies__year_from,
+        :general_assemblies__year_to)
+      .select{[:general_assemblies__ordinal,
+        :general_assemblies__year_from,
+        :general_assemblies__year_to,
+        Sequel.as(array_agg(:members_of_parliament_x_general_assemblies__member_of_parliament_id), :mp_ids),
+        Sequel.as(array_agg(distinct(:members_of_parliament_x_general_assemblies__party)), :parties)]}
+      .all
+
+      .inject({}) do |hash, data|
+        hash[data[:ordinal]] = data.except(:ordinal)
+        hash
+      end
+
+    parties_mps_hash = DB[:members_of_parliament_x_general_assemblies]
+      .group_by(:party)
+      .select{[:party,
+        Sequel.as(array_agg(:member_of_parliament_id), :mp_ids)]}
+      .all
+      .inject({}) do |hash, data|
+        hash[data[:party]] = data[:mp_ids]
+        hash
+      end
+
+    mps_name_hash = DB[:members_of_parliament].select_hash(:id, :name)
+
     render :json => {
-      general_assemblies: ['a', 'b'],
-      parties: ['1','2'],
-      mps: ['doddi', 'eysi']
+      ga_data_hash: ga_data_hash,
+      parties_mps_hash: parties,
+      mps_name_hash: mps_name_hash
     }
   end
 
